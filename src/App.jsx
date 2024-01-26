c9d0bcb0d2b9b21c602ba2009810cd6677346898
Edgaras <e.jurgelevicius@gmail.com>
2024-01-27 00:45:56 +0200

    styles correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
 import Control from './components/Control/Control'
+import Draggable from 'react-draggable'
+import TodoApp from './components/TodoApp/TodoApp'
 
 function App() {
   return (
     <>
+      <Draggable defaultPosition={{ x: 200, y: 1000 }} position={null}>
+        <div>
+          <TodoApp />
+        </div>
+      </Draggable>
       <Control />
     </>
   )
